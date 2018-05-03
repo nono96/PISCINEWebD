064ec39026185cbd4a22afe44dad2146eb96e3df
chloehdeb <chloe.hunck-de-boxtel@edu.ece.fr>
2018-05-03 18:39:53 +0200

    page html pour formulaire connexion

---
+++ login.php
@@ -0,0 +1,14 @@
+<h1>Connexion</h1>
+
+
+<form method="POST" action="connexionDuMembre.php">
+	<label for="email">Adresse mail:</label>
+	<input type="text" name="email"/><br/>
+
+	<label for="mdp">Mot de passe:</label>
+	<input type="password" name="mdp"/><br/>
+
+	<input type="submit" value="Se Connecter" name="submit"/>
+</form>
+
+<a href='index.php?page=register'>pas encore membre</a>
